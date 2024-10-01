f7922590c24b532827bfc56f2c091ae69d090331
memili_carrot <105141162+memili-carrot@users.noreply.github.com>
2024-10-01 16:28:30 +0900

    [Silver III] Title: 천재 수학자 성필, Time: 32 ms, Memory: 31120 KB -BaekjoonHub

---
+++ 백준/Silver/15815. 천재 수학자 성필/천재 수학자 성필.py
@@ -0,0 +1,20 @@
+def evaluate_postfix(expression):
+    stack = []
+    for char in expression:
+        if char.isdigit():
+            stack.append(int(char))
+        else:
+            b = stack.pop()
+            a = stack.pop()
+            if char == '+':
+                result = a + b
+            elif char == '-':
+                result = a - b
+            elif char == '*':
+                result = a * b
+            elif char == '/':
+                result = a // b
+            stack.append(result)
+    return stack[0]
+expression = input().strip()
+print(evaluate_postfix(expression))
