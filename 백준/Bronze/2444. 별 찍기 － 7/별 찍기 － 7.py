996d8cf8869292d719aab552b3d383408355d5f7
memili_carrot <105141162+memili-carrot@users.noreply.github.com>
2024-10-13 09:56:02 +0900

    [Bronze III] Title: 별 찍기 - 7, Time: 32 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Bronze/2444. 별 찍기 － 7/별 찍기 － 7.py
+++ 백준/Bronze/2444. 별 찍기 － 7/별 찍기 － 7.py
@@ -1,5 +1,3 @@
 N = int(input())
-for i in range(1, N + 1):
-    print(' ' * (N - i) + '*' * (2 * i - 1))
-for i in range(N - 1, 0, -1):
-    print(' ' * (N - i) + '*' * (2 * i - 1))
+for i in range(1-N, N, 1):
+    print(' '*abs(i)+'*'*abs(1-2*N+2*abs(i)))
