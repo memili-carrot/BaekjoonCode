66d3fdab6a34132042cd89294288e3c3916a5886
memili_carrot <105141162+memili-carrot@users.noreply.github.com>
2024-10-14 20:39:22 +0900

    [Silver I] Title: 스티커, Time: 440 ms, Memory: 52240 KB -BaekjoonHub

--- 백준/Silver/9465. 스티커/스티커.py
+++ 백준/Silver/9465. 스티커/스티커.py
@@ -1,36 +1,33 @@
+import sys
+input = sys.stdin.read
+
 def max_sticker_score(t, test_cases):
     results = []
-    for _ in range(t):
-        n = test_cases[_][0]
-        stickers = test_cases[_][1]
-        
+    for n, stickers in test_cases:
         if n == 1:
             results.append(max(stickers[0][0], stickers[1][0]))
             continue
-        
-        prev1_0 = stickers[0][0]
-        prev1_1 = stickers[1][0]
-        prev2_0 = 0
-        prev2_1 = 0
+        prev1_0, prev1_1 = stickers[0][0], stickers[1][0]
+        prev2_0, prev2_1 = 0, 0
         
         for i in range(1, n):
             current_0 = stickers[0][i] + max(prev1_1, prev2_1)
             current_1 = stickers[1][i] + max(prev1_0, prev2_0)
             prev2_0, prev2_1 = prev1_0, prev1_1
             prev1_0, prev1_1 = current_0, current_1      
-        results.append(max(prev1_0, prev1_1))  
+        results.append(max(prev1_0, prev1_1))
     return results
 
-t = int(input())
+data = input().splitlines()
+t = int(data[0])
 test_cases = []
+index = 1
 
 for _ in range(t):
-    n = int(input())
-    stickers = []
-    stickers.append(list(map(int, input().split())))
-    stickers.append(list(map(int, input().split())))
+    n = int(data[index])
+    stickers = [list(map(int, data[index + 1].split())), list(map(int, data[index + 2].split()))]
     test_cases.append((n, stickers))
+    index += 3
 
 results = max_sticker_score(t, test_cases)
-for result in results:
-    print(result)
+sys.stdout.write("\n".join(map(str, results)) + "\n")
