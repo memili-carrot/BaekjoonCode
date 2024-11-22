d529cb99416cbbff21cfc59d162f5d6e1e0b563d
memili_carrot <105141162+memili-carrot@users.noreply.github.com>
2024-11-23 04:33:46 +0900

    [Silver III] Title: 피보나치 함수, Time: 32 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Silver/1003. 피보나치 함수/피보나치 함수.py
+++ 백준/Silver/1003. 피보나치 함수/피보나치 함수.py
@@ -1,17 +1,21 @@
-max_n = 40
-count_0 = [0] * (max_n + 1)
-count_1 = [0] * (max_n + 1)
-count_0[0] = 1
-count_1[0] = 0
-count_0[1] = 0
-count_1[1] = 1
+def fibonacci_count(t, test_cases):
+    max_n = 40
+    count_0, count_1 = [0] * (max_n + 1), [0] * (max_n + 1)
+    count_0[0], count_1[0] = 1, 0
+    count_0[1], count_1[1] = 0, 1
 
-for i in range(2, max_n + 1):
-    count_0[i] = count_0[i - 1] + count_0[i - 2]
-    count_1[i] = count_1[i - 1] + count_1[i - 2]
+    for i in range(2, max_n + 1):
+        count_0[i] = count_0[i - 1] + count_0[i - 2]
+        count_1[i] = count_1[i - 1] + count_1[i - 2]
 
-T = int(input())
+    results = []
+    for n in test_cases:
+        results.append((count_0[n], count_1[n]))
+    return results
 
-for _ in range(T):
-    N = int(input())
-    print(count_0[N], count_1[N])
+t = int(input())
+test_cases = [int(input()) for _ in range(t)]
+output = fibonacci_count(t, test_cases)
+
+for res in output:
+    print(res[0], res[1])
