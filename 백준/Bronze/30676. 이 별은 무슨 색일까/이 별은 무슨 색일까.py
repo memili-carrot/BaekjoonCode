589dcc82b0212acc6f64453bee958c7544c92635
memili_carrot <105141162+memili-carrot@users.noreply.github.com>
2024-11-24 16:58:07 +0900

    [Bronze V] Title: 이 별은 무슨 색일까, Time: 36 ms, Memory: 31120 KB -BaekjoonHub

---
+++ 백준/Bronze/30676. 이 별은 무슨 색일까/이 별은 무슨 색일까.py
@@ -0,0 +1,16 @@
+length = int(input().strip())
+
+if 620 <= length <= 780:
+    print("Red")
+elif 590 <= length < 620:
+    print("Orange")
+elif 570 <= length < 590:
+    print("Yellow")
+elif 495 <= length < 570:
+    print("Green")
+elif 450 <= length < 495:
+    print("Blue")
+elif 425 <= length < 450:
+    print("Indigo")
+elif 380 <= length < 425:
+    print("Violet")
