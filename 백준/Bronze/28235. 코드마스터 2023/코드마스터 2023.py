15a055be95a1d0e56fee79d07d4c81b654569d84
memili_carrot <105141162+memili-carrot@users.noreply.github.com>
2024-11-24 14:51:48 +0900

    [Bronze V] Title: 코드마스터 2023, Time: 32 ms, Memory: 31120 KB -BaekjoonHub

---
+++ 백준/Bronze/28235. 코드마스터 2023/코드마스터 2023.py
@@ -0,0 +1,9 @@
+cheer = input()
+if cheer == "SONGDO":
+    print("HIGHSCHOOL")
+elif cheer == "CODE":
+    print("MASTER")
+elif cheer == "2023":
+    print("0611")
+elif cheer == "ALGORITHM":
+    print("CONTEST")
