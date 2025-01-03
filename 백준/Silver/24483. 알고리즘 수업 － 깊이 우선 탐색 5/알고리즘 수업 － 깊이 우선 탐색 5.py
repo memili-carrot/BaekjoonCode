fb2770b2864568b66dce92efb5ada33e3b9a5b1c
memili_carrot <105141162+memili-carrot@users.noreply.github.com>
2025-01-03 22:32:34 +0900

    [Silver II] Title: 알고리즘 수업 - 깊이 우선 탐색 5, Time: 664 ms, Memory: 76552 KB -BaekjoonHub

---
+++ 백준/Silver/24483. 알고리즘 수업 － 깊이 우선 탐색 5/알고리즘 수업 － 깊이 우선 탐색 5.py
@@ -0,0 +1,43 @@
+import sys
+
+sys.setrecursionlimit(10 ** 6)
+input = sys.stdin.readline
+
+def dfs(node, depth_value):
+    global visit_order
+    depth[node] = depth_value
+    order[node] = visit_order
+    visit_order += 1
+
+    for nxt in graph[node]:
+        if depth[nxt] == -1:
+            dfs(nxt, depth_value + 1)
+
+
+def main():
+    n, m, r = map(int, input().split())
+
+    global graph, depth, order, visit_order
+    graph = [[] for _ in range(n + 1)]
+    depth = [-1] * (n + 1)
+    order = [0] * (n + 1)
+    visit_order = 1
+
+    for _ in range(m):
+        u, v = map(int, input().split())
+        graph[u].append(v)
+        graph[v].append(u)
+
+    for i in range(1, n + 1):
+        graph[i].sort()
+    dfs(r, 0)
+
+    answer = 0
+    for i in range(1, n + 1):
+        answer += depth[i] * order[i]
+
+    print(answer)
+
+
+if __name__ == "__main__":
+    main()
