faa5e94a517740f418d4d06c8f29850fd87a4a09
memili_carrot <105141162+memili-carrot@users.noreply.github.com>
2025-01-05 05:18:57 +0900

    [Silver II] Title: 알고리즘 수업 - 너비 우선 탐색 1, Time: 460 ms, Memory: 61520 KB -BaekjoonHub

---
+++ 백준/Silver/24444. 알고리즘 수업 － 너비 우선 탐색 1/알고리즘 수업 － 너비 우선 탐색 1.py
@@ -0,0 +1,42 @@
+import sys
+from collections import deque
+
+input = sys.stdin.readline
+
+def bfs(start):
+    visit_order = 1
+    visited[start] = True
+    order[start] = visit_order
+    queue = deque([start])
+
+    while queue:
+        u = queue.popleft()
+        for v in graph[u]:
+            if not visited[v]:
+                visited[v] = True
+                visit_order += 1
+                order[v] = visit_order
+                queue.append(v)
+
+def main():
+    n, m, r = map(int, input().split())
+
+    global graph, visited, order
+    graph = [[] for _ in range(n + 1)]
+    visited = [False] * (n + 1)
+    order = [0] * (n + 1)
+
+    for _ in range(m):
+        u, v = map(int, input().split())
+        graph[u].append(v)
+        graph[v].append(u)
+
+    for i in range(1, n + 1):
+        graph[i].sort()
+    bfs(r)
+
+    for i in range(1, n + 1):
+        print(order[i])
+
+if __name__ == "__main__":
+    main()
